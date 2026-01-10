445630e6d21f38ff5453d50d896913d2f780db3f
Rakesh Yadav <ry7955724@gmail.com>
2026-01-10 19:13:23 +0530

    updated work

---
+++ Day-12/video 56/index.js
@@ -0,0 +1,12 @@
+console.log("Hello I am conditional tutorial")
+
+let age =17;
+let grace =2;
+if(age+grace>18){
+    console.log("You can drive");
+
+}
+else{
+    console.log("You cannot drive");
+
+}
