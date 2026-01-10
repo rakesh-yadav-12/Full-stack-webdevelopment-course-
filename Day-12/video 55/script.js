39c7fa3a2859c5ee993545dc7cadc06335af4961
Rakesh Yadav <ry7955724@gmail.com>
2026-01-10 15:26:25 +0530

    updated video 55

---
+++ Day-12/video 55/script.js
@@ -0,0 +1,32 @@
+console.log("Hey this is tutorial 55")
+
+var a=5;
+var b=6;
+var c="Rakesh";
+
+console.log(a+b+8)
+console.log(typeof a, typeof b, typeof c)
+
+// const a1=6;
+// a1=a1+1;
+
+// not allowed because a1 is const
+
+let x = "Rakesh bhai";
+let y= 22;
+let z= 3.55;
+const p=true;
+let q =undefined;
+let r =null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r,c)
+
+let o= {
+    "name": "Rakesh Yadav",
+    "job code":5600,
+"is_handsome":true
+}
+console.log(o)
+o.salary ="100crores"
+console.log(o)
