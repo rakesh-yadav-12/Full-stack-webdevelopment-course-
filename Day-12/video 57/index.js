87f0ddc3147a0253bcf07735c7af747b106bf3f2
Rakesh Yadav <ry7955724@gmail.com>
2026-01-10 19:53:05 +0530

    updated Day-12 work

---
+++ Day-12/video 57/index.js
@@ -0,0 +1,37 @@
+// console.log("I am a tutorial on Loops")
+// let a=1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 50; i++) {
+//     console.log(a+i);
+    
+// }   
+// let obj ={
+//     name: "Rakesh",
+//     role:"Programmer",
+//     company:"CodeWithRakesh AI"
+
+// }
+// for(const key in obj){
+//     const element= obj[key];
+//     console.log(key,element)
+// }
+
+// for(const c of "Harry"){
+//     console.log(c)
+// }
+
+
+// let i =0;
+// while(i<6){
+//     console.log(i)
+//     i++;
+// }
+
+// let i=7;
+// do{
+//     console.log(i)
+//     i++;
+// }while(i<6);
