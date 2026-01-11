3bbc0a9fbe9569673c9943312876457ef4895e79
Rakesh Yadav <ry7955724@gmail.com>
2026-01-11 20:25:31 +0530

    updated Day-13 work

---
+++ Day-13/video 58/index.js
@@ -0,0 +1,24 @@
+// function nice(name){
+// console.log("Hey "+ name+ "  you are nice!")
+// console.log("Hey "+ name+ " you are nice!")
+// console.log("Hey "+ name+ " you are good!")
+// console.log("Hey "+ name+ " your tshirt is nice1")
+// console.log("Hey "+ name+ " your course is good too!")
+// }
+
+// nice("Rakesh Yadav")
+
+function sum( a, b, c=3){
+    // console.log(a+b)
+    return a+b+c
+
+}
+
+result1= sum(3,5)
+result2= sum(7,5)
+result3= sum(3,13,1)
+
+console.log("The sum of these numbers is :" , result1 ,result2, result3)
+
+
+
