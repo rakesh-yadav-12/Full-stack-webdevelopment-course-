2acdac1cedaa8b8fa9f76726cfa4bd15b615bce0
Rakesh Yadav <ry7955724@gmail.com>
2026-01-13 21:41:46 +0530

    updated spotify clone

---
+++ Day-13/video 60/index.js
@@ -0,0 +1,15 @@
+console.log("This is strings tutorial")
+let a= "Harry";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+
+console.log(a.length)
+
+let real_name ="Harry"
+let friend ="Rohan"
+// console.log("His name is"+ name + "and his frined name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
---
+++ Day-14/video-61/index.js
@@ -0,0 +1,5 @@
+let random= Math.random()
+if(random>0.1){
+    
+}
+
